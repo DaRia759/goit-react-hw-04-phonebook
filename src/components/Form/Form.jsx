dec54268fe1a339580afb542a13588d4d603349e
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-09-10 19:55:47 +0200

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,28 +2,33 @@ import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import css from './Form.module.css';
 
-
-export const Form = (onAddContact) => {
+export const Form = ({ onAddContact }) => {
     const [name, setName] = useState('');
     const [number, setNumber] = useState('');
 
     const handleChange = e => {
-        const { name, number, value } = e.currentTarget;
-        setName({ [name]: value });
-        setNumber({ [number]: value });
+        const { name, value } = e.target;
+        if (name === 'name') {
+            setName(value);
+        } else if (name === 'number') {
+            setNumber(value);
+        }
     };
+
     const handleFormSubmit = e => {
         e.preventDefault();
-        onAddContact({name, number});
+        onAddContact({ name, number });
         reset();
     };
+
     const reset = () => {
-        this.setState({ name: '', number: '' });
+        setName('');
+        setNumber('');
     };
 
     return (
         <form className={css.form} onSubmit={handleFormSubmit}>
-            <label className={css.label}><span className={css.span}>Name</span>
+                        <label className={css.label}><span className={css.span}>Name</span>
             <input className={css.input}
                 type="text"
                 name="name"
@@ -46,64 +51,12 @@ export const Form = (onAddContact) => {
                     />
             </label>
             <button className={css.button}type="submit">Add contact</button>
-            </form>
-        );
+        </form>
+    );
 };
-
-// class Form extends React.Component {
-//     state = {
-//         name: '',
-//         number: '',
-//     };
-//     handleChange = e => {
-//         const { name, value } = e.currentTarget;
-//         this.setState({ [name]: value });
-//     };
-//     handleFormSubmit = e => {
-//         e.preventDefault();
-//         this.props.onAddContact(this.state);
-//         this.reset();
-//     };
-//     reset = () => {
-//         this.setState({ name: '', number: '' });
-//     };
-
-
-//     render() {
-//         return (
-//             <form className={css.form} onSubmit={this.handleFormSubmit}>
-//                 <label className={css.label}><span className={css.span}>Name</span>
-//                     <input className={css.input}
-//                         type="text"
-//                         name="name"
-//                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//                         required
-//                         value={this.state.name}
-//                         onChange={this.handleChange}
-//                     />
-//                 </label>
-//                 <label className={css.label}><span className={css.span}>Number</span>
-//                     <input className={css.inputNumber}
-//                         type="tel"
-//                         name="number"
-//                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//                         required
-//                         value={this.state.number}
-//                         onChange={this.handleChange}
-//                     />
-//                 </label>
-//                 <button className={css.button}type="submit">Add contact</button>
-//             </form>
-//         );
-//     }
-// }
 
 Form.propTypes = {
     onAddContact: PropTypes.func.isRequired,
-    // onSubmit: PropTypes.func.isRequired,
-    // handleChange: PropTypes.func.isRequired,
 };
-    
+
 export default Form;
